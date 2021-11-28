647944acf5aea3a90d9b5c32b8f842278c286ef0
Aleksandr Soloshenko <capcom@soft-c.ru>
2021-11-28 21:31:05 +0700

    Added: connection between video streaming and video storage

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const http = require('http');
 const fs = require('fs');
 const app = express();
 
@@ -9,29 +10,36 @@ require('dotenv').config()
 if (!process.env.PORT) {
     throw new Error('Please specify the port number for the HTTP server with the enviroment variable PORT.');
 }
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error('Please specify video storage address with the enviroment variable VIDEO_STORAGE_HOST.');
+}
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error('Please specify video storage port with the enviroment variable VIDEO_STORAGE_PORT.');
+}
 
 const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Node Video Streaming Service');
 });
 
 app.get('/video', (req, res) => {
-    const filename = path.join(__dirname, '../videos/SampleVideo_1280x720_30mb.mp4');
-    res.sendFile(filename);
-    // fs.stat(path, (err, stats) => {
-    //     if (err) {
-    //         console.error('An error occured');
-    //         res.sendStatus(500);
-    //         return;
-    //     }
+    const forwardRequest = http.request({
+        host: VIDEO_STORAGE_HOST,
+        port: VIDEO_STORAGE_PORT,
+        path: '/video?path=SampleVideo_1280x720_30mb.mp4',
+        method: 'GET',
+        headers: req.headers
+    }, forwardResponse => {
+        res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+        forwardResponse.pipe(res);
+    });
 
-    //     res.writeHead(200, { 'Content-Length': stats.size, 'Content-Type': 'video/mp4' });
-
-    //     fs.createReadStream(path).pipe(res);
-    // });
+    req.pipe(forwardRequest);
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listeting on port ${PORT}!`);
+    console.log(`Video streaming service is listeting on port ${PORT}!`);
 });
