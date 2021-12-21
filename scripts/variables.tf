9a1e441c5edb27d020bca2c54a88c0e93102d19c
Alexander Soloshenko <capcom@soft-c.ru>
2021-12-21 09:50:06 +0700

    Rollback: use only vars

--- scripts/variables.tf
+++ scripts/variables.tf
@@ -1,15 +1,23 @@
-variable "app_name" {
+# Sets global variables for this Terraform project.
+
+variable app_name {
     default = "nodetube"
 }
-
-variable "location" {
-    default = "West Europe"
+variable location {
+  default = "West Europe"
 }
 
 variable admin_username {
   default = "linux_admin"
 }
 
-variable app_version {
-  
+variable app_version { # Can't be called version! That's a reserved word.
+}
+
+variable client_id {
+
+}
+
+variable client_secret {
+
 }
